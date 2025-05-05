09e9c2e817809975c87914c9a44be1e3994013ea
larrybah <larrybah@gmail.com>
2025-05-05 21:17:38 +0000

    script file removed, and contact section edited

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('formResponse').textContent = "Thanks for reaching out! I'll get back to you soon.";
-  this.reset();
-});
